f1dd3aab881c55a825c096c51644dca821bd751c
César Viana Barros <126491073+CVbarro@users.noreply.github.com>
2023-11-21 15:19:57 -0300

    Trabalho semifinalizado

--- Academia/src/Dao/ClienteDao.java
+++ Academia/src/Dao/ClienteDao.java
@@ -1,5 +1,32 @@
 package Dao;
 
+import entidades.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class ClienteDao {
-    
+    private Connection connection;
+
+    public ClienteDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void realizarCadastro(Cliente cliente) {
+        String sql = "INSERT INTO cliente (nome, cpf, cnpj, email, data_nascimento, matricula) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, cliente.getNome());
+            preparedStatement.setString(2, cliente.getCpf());
+            preparedStatement.setString(3, cliente.getCnpj());
+            preparedStatement.setString(4, cliente.getEmail());
+            preparedStatement.setString(5, cliente.getData_nascimento());
+            preparedStatement.setInt(6, cliente.getMatricula());
+
+            preparedStatement.executeUpdate();
+            System.out.println("Cliente cadastrado com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- Academia/src/entidades/Pagamento.java
+++ Academia/src/entidades/Pagamento.java
@@ -2,12 +2,10 @@ package entidades;
 
 public class Pagamento {
     private String idPagamento, dataPagamento;
-    private boolean status;
 
-    public Pagamento(String idPagamento, String dataPagamento, boolean status){
+    public Pagamento(String idPagamento, String dataPagamento){
         this.dataPagamento = dataPagamento;
         this.idPagamento = idPagamento;
-        this.status = status;
 
     }
 
@@ -23,9 +21,6 @@ public class Pagamento {
     }
     public void setIdPagamento(String idPagamento) {
         this.idPagamento = idPagamento;
-    }
-    public void setStatus(boolean status) {
-        this.status = status;
     }
 
 }
