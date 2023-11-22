5d603dddd8590500dae1eaad4c93d92a52291c53
mncbl <mateusncbl@gmail.com>
2023-11-21 21:24:34 -0300

    mateus

--- Academia/src/Main.java
+++ Academia/src/Main.java
@@ -7,18 +7,23 @@ public class Main {
     public static void main(String[] args) {
         Conexao conexao = new Conexao();
         Connection connection = conexao.recuperaConexao();
+    try {
 
         Cliente cliente = new Cliente("Nome", "12345678900", "12345678901234", "email@example.com", "01/01/2000",
                 1001);
         ClienteDao clienteDao = new ClienteDao(connection);
 
         clienteDao.realizarCadastro(cliente);
-
+    }
         // Lembre-se de fechar a conexão quando terminar de usá-la
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
